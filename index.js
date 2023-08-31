8306021aac2dad32e6d49d1e6ea4b8e2b8272281
lindakahju <116061883+lindakahju@users.noreply.github.com>
2023-08-31 09:56:08 +0200

    css

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const validPaths = '/home'; 
+// const validPaths = '/home'; 
 
-let isConditionMet = true; // Change this to false to see the redirection to the error page
-const checkURLPath = () => {
-    const currentPath = window.location.pathname;
-    if (!validPaths.includes(currentPath)) {
-       isConditionMet = false;
-    }
-};
+// let isConditionMet = true; // Change this to false to see the redirection to the error page
+// const checkURLPath = () => {
+//     const currentPath = window.location.pathname;
+//     if (!validPaths.includes(currentPath)) {
+//        isConditionMet = false;
+//     }
+// };
 
-// Call the function when the page loads
-//window.onload = checkURLPath;
+// // Call the function when the page loads
+// //window.onload = checkURLPath;
 
 
-        if (isConditionMet) {
-            // Redirect to index.html
-            window.location.href = "home.html";
-        } else {
-            // Redirect to error.html
-            window.location.href = "error.html";
-        }
+//         if (isConditionMet) {
+//             // Redirect to index.html
+//             window.location.href = "home.html";
+//         } else {
+//             // Redirect to error.html
+//             window.location.href = "error.html";
+//         }
