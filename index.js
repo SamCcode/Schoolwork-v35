7fd1a6eb21ebfafe231d91cc71e801e11a90f50c
SamCcode <yhfe22.karlsa@folkuniversitetet.nu>
2023-08-31 13:21:01 +0200

    js added

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ const validPaths = '/home.html';
     };
 
     window.onload = checkURLPath;
+    const imageContainer = document.getElementById("imageContainer");
+    // Load JSON data using fetch
+    fetch('dogImg.json')
+    .then(response => response.json())
+    .then(jsonData => {
+        // Loop through the JSON data and create <img> elements for each image
+        jsonData.forEach(imageData => {
+            let el = document.createElement("article");
+            el.innerHTML = (`
+            <img src="${imageData.img}" class="">
+                <h3>Dog</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            `);
+          
+          imageContainer.appendChild(el);
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching JSON data:', error);
+      });
+    
